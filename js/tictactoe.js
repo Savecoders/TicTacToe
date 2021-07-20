0770acedd4a7996d67e8a4dee94d746da1a686c3
Savecoders <pablopincay2003@gmail.com>
2021-07-19 19:42:05 -0500

    final version TicTacToe and Dark Mode integrated

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,37 +1,72 @@
-const board = document.getElementById('thisBoard');
-board.childNodes.forEach((Element) => {
-	Element.addEventListener('click', () => {
-		if (!Element.textContent) {
-			Element.textContent = '✔';
-			let ramdowBool;
-			let availablePositions = 0;
-			for (let i = 0; i < 9; i++) {
-				if (!board.children[i].textContent) {
-					availablePositions++;
-				}
-			}
-			console.log(availablePositions);
-			do {
-				let numRamdom = Math.floor(Math.random() * (8 - 0 + 1) + 0);
-				if (
-					!(
-						board.children[numRamdom].textContent == '✔' ||
-						board.children[numRamdom].textContent == '✖'
-					)
-				) {
-					board.children[numRamdom].textContent = '✖';
-					console.log(numRamdom);
-					ramdowBool = true;
-				} else {
-					ramdowBool = false;
-				}
-				if (availablePositions < 2) {
-					console.log('empate');
-					ramdowBool = true;
-				}
-			} while (!ramdowBool);
+const boxes = document.querySelectorAll('.box');
+const message = document.getElementById('message');
+const restart = document.getElementById('restart');
+const playerIcon = '✖';
+const botIcon = '';
+const board = new Array(9).fill(null);
+const combinationsWins = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+
+const getRandomInt = (min, max) => {
+	return Math.floor(Math.random() * (max - min)) + min;
+};
+const startGame = (e) => {
+	if (!e.target.textContent) {
+		const id = e.target.id;
+		board[id] = playerIcon;
+		e.target.innerText = playerIcon;
+		if (checkForWin(playerIcon)) {
+			endgame(playerIcon);
+		} else if (!board.some((box) => box === null)) {
+			message.innerHTML = `No Winner`;
 		} else {
-			console.log('no se  envio el dato, casillero lleno');
+			boxDrawBot();
 		}
+		if (checkForWin(botIcon)) {
+			endgame(botIcon);
+		}
+	}
+};
+function boxDrawBot() {
+	let boolStateRandowPoss = false;
+	do {
+		let randomNum = getRandomInt(0, 8);
+		if (!board[randomNum]) {
+			boxes[randomNum].textContent = botIcon;
+			board[randomNum] = botIcon;
+			boolStateRandowPoss = true;
+		}
+	} while (!boolStateRandowPoss);
+}
+
+const checkForWin = (Icon) => {
+	return combinationsWins.some((combination) => {
+		return combination.every((comb) => {
+			return board[comb] === Icon;
+		});
 	});
-});
+};
+
+const endgame = (Icon) => {
+	message.innerHTML = `Winner ${Icon}`;
+	boxes.forEach((box) => box.removeEventListener('click', startGame));
+};
+
+const reset = () => {
+	message.innerHTML = `Lets's GO!`;
+	boxes.forEach((box) => (box.textContent = ''));
+	boxes.forEach((box) => box.addEventListener('click', startGame));
+	board.fill(null);
+};
+
+boxes.forEach((box) => box.addEventListener('click', startGame));
+
+restart.addEventListener('click', reset);
